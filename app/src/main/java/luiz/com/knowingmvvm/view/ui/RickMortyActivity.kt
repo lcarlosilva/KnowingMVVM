45744298e06ef3c4ae24f9f04e0a8e2d678f2797
Luiz Arruda <luiz.arruda@ifood.com.br>
2022-04-12 01:16:56 -0300

    extract method of RecyclerView

--- app/src/main/java/luiz/com/knowingmvvm/view/ui/RickMortyActivity.kt
+++ app/src/main/java/luiz/com/knowingmvvm/view/ui/RickMortyActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_rick_morty.*
 import luiz.com.knowingmvvm.R
+import luiz.com.knowingmvvm.domain.entity.Character
 import luiz.com.knowingmvvm.view.adapter.CharacterAdapter
 import luiz.com.knowingmvvm.view.view_model.RickMortyViewModel
 import org.koin.androidx.viewmodel.ext.android.viewModel
@@ -19,10 +20,15 @@ class RickMortyActivity : AppCompatActivity() {
         setContentView(R.layout.activity_rick_morty)
 
         viewModel.characterLiveData.observe(this, {
-            rv_character.adapter = CharacterAdapter(it)
-            rv_character.setHasFixedSize(true)
-            rv_character.layoutManager = LinearLayoutManager(this@RickMortyActivity)
+            buildRecycler(it)
         })
+
         viewModel.characters()
+    }
+
+    private fun buildRecycler(it: ArrayList<Character>) {
+        rv_character.adapter = CharacterAdapter(it)
+        rv_character.setHasFixedSize(true)
+        rv_character.layoutManager = LinearLayoutManager(this@RickMortyActivity)
     }
 }
